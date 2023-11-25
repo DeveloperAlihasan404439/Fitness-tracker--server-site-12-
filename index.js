d8c37ec902af619c83212824216baaa166a7182b
alihasanlitonislam <alihasan404439@gmail.com>
2023-11-25 23:33:11 +0600

    added the gallery api successfull data load

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
-const express = require('express')
-const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.port || 5000;
 require("dotenv").config();
 
-app.use(cors())
-app.use(express.json())
-
-
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wjgws1x.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -18,26 +16,40 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
-    // TODO: remove the client connect 
-    await client.connect();
+    // TODO: remove the client connect
+    // await client.connect();
     // Send a ping to confirm a successful connection
+    const galleryCollection = client.db("bodyPulse").collection("gallery");
+
+    app.get("/gallery", async (req, res) => {
+      const { offset = 0, limit = 12 } = req.query;
+      const result = await galleryCollection
+        .find()
+        .skip(parseInt(offset))
+        .limit(parseInt(limit))
+        .toArray();
+      res.send(result);
+    });
+
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
 
-app.get('/', (req, res)=>{
-    res.send('Fitness Tracker Website')
-})
-app.listen(port, ()=>{
-    console.log(`server site start the port ${port}`)
-})
+app.get("/", (req, res) => {
+  res.send("Fitness Tracker Website");
+});
+app.listen(port, () => {
+  console.log(`server site start the port ${port}`);
+});
